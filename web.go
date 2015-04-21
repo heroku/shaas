27c6eb0b6a5bb1b92017ac4b7587b73a8bf4e0ed
Ryan Brainard <brainard@heroku.com>
2015-04-21 09:09:41 -0700

    Extract file open to handleAll

--- web.go
+++ web.go
@@ -15,22 +15,74 @@ func main() {
 }
 
 func Start(port string) {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		switch req.Method {
-		case "GET":
-			handleGet(res, req)
-			//		case "POST":
-			//			handlePost(res, req)
-		default:
-			http.Error(res, "Only GET and POST supported", http.StatusMethodNotAllowed)
-		}
-	})
+	http.HandleFunc("/", handleAll)
 
 	if port == "" {
 		port = "5000"
 	}
 	log.Println("listening on port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func handleAll(res http.ResponseWriter, req *http.Request) {
+	log.Printf("method=%s path=%q", req.Method, req.URL.Path)
+
+	path, err := os.Open(req.URL.Path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			handleError(res, req, err, http.StatusNotFound, "File not found")
+			return
+		}
+
+		handleError(res, req, err, http.StatusBadRequest, "Error reading path")
+		return
+	}
+	defer path.Close()
+
+	switch req.Method {
+	case "GET":
+		handleGet(res, req, path)
+	//		case "POST":
+	//			handlePost(res, req)
+	default:
+		http.Error(res, "Only GET and POST supported", http.StatusMethodNotAllowed)
+	}
+}
+
+func handleGet(res http.ResponseWriter, req *http.Request, path *os.File) {
+	pathInfo, err := path.Stat()
+	if err != nil {
+		handleError(res, req, err, http.StatusInternalServerError, "Error reading path info")
+		return
+	}
+
+	if pathInfo.Mode().IsDir() {
+		fileInfos, err := path.Readdir(0)
+		if err != nil {
+			handleError(res, req, err, http.StatusInternalServerError, "Error reading directory")
+			return
+		}
+
+		fileResponses := []fileInfoResponse{}
+		for _, fi := range fileInfos {
+			fileResponses = append(fileResponses, toFileInfoResponse(fi))
+		}
+
+		res.Header().Set("Content-Type", "application/json")
+		fileResponsesJson, err := json.Marshal(fileResponses)
+		if err != nil {
+			handleError(res, req, err, http.StatusInternalServerError, "Error serializing response")
+			return
+		}
+		res.Write(fileResponsesJson)
+	} else if pathInfo.Mode().IsRegular() {
+		io.Copy(res, path)
+	} else {
+		handleError(res, req,
+			fmt.Errorf("Invalid file type: "+string(pathInfo.Mode().String()[0])),
+			http.StatusBadRequest,
+			"Invalid file type")
+	}
 }
 
 type fileInfoResponse struct {
@@ -72,73 +124,3 @@ func handleError(res http.ResponseWriter, req *http.Request, err error, httpStat
 	res.Header().Set("Content-Type", "application/json")
 	http.Error(res, string(stdErrorJson), httpStatus)
 }
-
-func handleGet(res http.ResponseWriter, req *http.Request) {
-	path, err := os.Open(req.URL.Path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			handleError(res, req, err, http.StatusNotFound, "File not found")
-			return
-		}
-
-		handleError(res, req, err, http.StatusBadRequest, "Error reading path")
-		return
-	}
-
-	pathInfo, err := path.Stat()
-	if err != nil {
-		handleError(res, req, err, http.StatusInternalServerError, "Error reading path info")
-		return
-	}
-
-	if pathInfo.Mode().IsDir() {
-		fileInfos, err := path.Readdir(0)
-		if err != nil {
-			handleError(res, req, err, http.StatusInternalServerError, "Error reading directory")
-			return
-		}
-
-		fileResponses := []fileInfoResponse{}
-		for _, fi := range fileInfos {
-			fileResponses = append(fileResponses, toFileInfoResponse(fi))
-		}
-
-		res.Header().Set("Content-Type", "application/json")
-		fileResponsesJson, err := json.Marshal(fileResponses)
-		if err != nil {
-			handleError(res, req, err, http.StatusInternalServerError, "Error serializing response")
-			return
-		}
-		res.Write(fileResponsesJson)
-	} else if pathInfo.Mode().IsRegular() {
-		io.Copy(res, path)
-	} else {
-		handleError(res, req,
-			fmt.Errorf("Invalid file type: "+string(pathInfo.Mode().String()[0])),
-			http.StatusBadRequest,
-			"Invalid file type")
-	}
-}
-
-//func handlePost(res http.ResponseWriter, req *http.Request) {
-//	scheme := "http"
-//	if req.TLS != nil || req.Header.Get("X-Forwarded-Proto") == "https" {
-//		scheme = "https"
-//	}
-//
-//	data, err := ioutil.ReadAll(req.Body)
-//	if err != nil {
-//		log.Println("post.error.body:", err)
-//		http.Error(res, "Error reading request body: "+err.Error(), http.StatusBadRequest)
-//		return
-//	}
-//
-//	if err := handleStatusParam(res, req); err != nil {
-//		return
-//	}
-//
-//	res.Header().Set("Content-Type", "text/uri-list; charset=utf-8")
-//	contentType := req.Header.Get("Content-Type")
-//	uri := shaas.CreateUri(scheme, req.Host, req.URL.Path, contentType, data)
-//	fmt.Fprintln(res, uri)
-//}
