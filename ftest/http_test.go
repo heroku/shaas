9103a8abd74819a0a85efb9be5f4a66c7cfadcd7
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2020-10-28 12:32:44 +0900

    Test read only

--- ftest/http_test.go
+++ ftest/http_test.go
@@ -70,3 +70,15 @@ func TestPostDir(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, strings.TrimPrefix(env.fixturesUrl(env.services[ServiceDefault]), env.baseUrl(env.services[ServiceDefault]))+"\n", string(body))
 }
+
+func TestReadonlyAllowsGet(t *testing.T) {
+	res, err := http.Get(env.fixturesUrl(env.services[ServiceReadonly]) + "/a")
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+}
+
+func TestReadonlyForbidsPost(t *testing.T) {
+	res, err := http.Post(env.fixturesUrl(env.services[ServiceReadonly])+"/a", "", nil)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusMethodNotAllowed, res.StatusCode)
+}
